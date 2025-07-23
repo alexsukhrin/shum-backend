f9b48d94b656ddbbaf2435483c17c91807946787
Олександр Сухрин <o.sukhryn@temabit.com>
2025-07-23 14:34:07 +0300

    Initial commit: Django marketplace project setup

---
+++ shum/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from shum.users.models import User
+from shum.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
